8abe4b8c05f619362c1529151068cdbbdec9dabf
darse73 <k.yu.713558.2016113@gmail.com>
2022-02-11 03:07:56 +0900

    いったん保存

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+<div id="app">
+   <Header />
+  <div class="div">
+   </div>
   </div>
-  <router-view />
+
 </template>
 
+<script>
+import { defineComponent } from '@vue/composition-api'
+import Header from "@/components/Header.vue"
+
+export default defineComponent({
+  components: {
+    Header
+  },
+  setup() {
+    
+  },
+})
+</script>
+
+
 <style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+*
+  margin: 0
+  padding: 0
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="header"></div>
+</template>
+
+<style lang="stylus" scoped>
+.header
+    background-color: red
+    height: 100vh
+</style>

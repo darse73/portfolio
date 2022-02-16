221c74c9bf6df75262d40abc67c30b2638de39df
darse73 <k.yu.713558.2016113@gmail.com>
2022-02-16 22:54:28 +0900

    波の仕組みを作りました。

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-<div id="app">
-   <Header />
-  <div class="div">
-   </div>
+  <div id="app">
+    <Header />
+    <div class="div"></div>
   </div>
-
 </template>
 
 <script>
-import { defineComponent } from '@vue/composition-api'
-import Header from "@/components/Header.vue"
+import { defineComponent } from "vue";
+import Header from "@/components/Header.vue";
 
 export default defineComponent({
   components: {
-    Header
+    Header,
   },
-  setup() {
-    
-  },
-})
+});
 </script>
 
-
-<style lang="stylus">
+<style lang="sass">
 *
   margin: 0
   padding: 0
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,66 @@
 <template>
-    <div class="header"></div>
+  <div class="header">
+    <div class="title-top">DARSE</div>
+    <div class="title-bottom">Portfolio Of Kodama Yuya</div>
+    <div class="slide"><img src="" alt="" class="slide-img" /></div>
+    <Wave />
+  </div>
 </template>
 
-<style lang="stylus" scoped>
+<script lang="ts">
+import { defineComponent } from "vue";
+import Wave from "@/components/Wave.vue";
+
+export default defineComponent({
+  components: {
+    Wave,
+  },
+});
+</script>
+
+
+<style lang="sass" scoped>
 .header
-    background-color: red
-    height: 100vh
+  height: 100vh
+  position: relative
+
+@mixin title
+  transform: rotate(-15deg)
+  position: absolute
+
+.title-top
+  @include title
+  top: 31%
+  left: 14%
+  font-size: 4.5rem
+  letter-spacing: 0.5rem
+.title-bottom
+  @include title
+  top: 47%
+  left: 18%
+
+.slide
+  border: 0.1rem solid black
+  width: 30rem
+  height: 17rem
+  position: absolute
+  top: 15%
+  left: 50%
+
+  @mixin allow
+    content: ""
+    position: absolute
+    top: 50%
+    transform: translateY(-50%)
+    border-top: 1rem solid transparent
+    border-bottom: 1rem solid transparent
+
+  &:before
+    @include allow
+    left: -5rem
+    border-right: 1rem solid black
+  &:after
+    @include allow
+    right: -5rem
+    border-left: 1rem solid black
 </style>

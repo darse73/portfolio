942f605ca3ec3db175260c2e93f29f969cca2513
darse73 <k.yu.713558.2016113@gmail.com>
2022-02-26 13:47:22 +0900

    headerが白でmainが水色

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="main">
-    <div class="flame-t"></div>
-    <div class="flame-r"></div>
-    <div class="flame-b"></div>
-    <div class="flame-l"></div>
-    <div class="flame-up-l"></div>
-    <div class="flame-up-r"></div>
-    <div class="flame-low-r"></div>
-    <div class="flame-low-l"></div>
     <div class="contents">
       <div class="works-sec">
-        <div class="works-title bubble">WORKS</div>
-        <div class="work1 work bubble"><img src="" alt="" /></div>
-        <div class="work2 work bubble"><img src="" alt="" /></div>
+        <div class="works-title-wrap">
+          <div class="works-title bubble">WORKS</div>
+        </div>
+        <div class="work-wrap">
+          <div class="work1-wrap">
+            <div class="work1 work bubble"><img src="" alt="" /></div>
+          </div>
+          <div class="work2 work bubble"><img src="" alt="" /></div>
+        </div>
       </div>
     </div>
   </div>
@@ -27,73 +25,48 @@ export default defineComponent({});
 <style lang="sass" scoped>
 $base-color: #99CCFF
 
-@mixin grid-child($column, $row, $bg)
-  grid-column: $column
-  grid-row: $row
-  background: $bg
-  opacity: 0.75
-
-.main
-  display: grid
-  grid-template-columns: 20% 1fr 20%
-  grid-template-rows: 20rem 100rem 20rem
-
-.flame-t
-  @include grid-child(2, 1, linear-gradient($base-color, lighten($base-color, 15%)))
-
-.flame-r
-  @include grid-child(3, 2, linear-gradient(to left, $base-color, lighten($base-color, 15%)))
-
-.flame-b
-  @include grid-child(2, 3, linear-gradient(to top, $base-color, lighten($base-color, 15%)))
-
-.flame-l
-  @include grid-child(1, 2, linear-gradient(to right, $base-color, lighten($base-color, 15%)))
-
-.flame-up-l
-  @include grid-child(1, 1, radial-gradient(at 100% 100%, lighten($base-color, 15%), $base-color 70%))
-
-.flame-up-r
-  @include grid-child(3, 1, radial-gradient(at 0% 100%, lighten($base-color, 15%), $base-color 70%))
-
-.flame-low-r
-  @include grid-child(3, 3, radial-gradient(at 0% 0%, lighten($base-color, 15%), $base-color 70%))
-
-.flame-low-l
-  @include grid-child(1, 3, radial-gradient(at 100% 0%, lighten($base-color, 15%), $base-color 70%))
-
 .contents
-  grid-column: 2
-  grid-row: 2
   opacity: 0.75
-  background-color: lighten($base-color, 15%)
+  background-color: $base-color
 
 .bubble
-  background: radial-gradient(lighten($base-color, 15%) 50%, $base-color 100%)
+  background: radial-gradient(white 50%, $base-color 60%)
   border-radius: 50%
   text-align: center
 
 .works-sec
-  position: relative
+  display: flex
+  height: 200vh
+
+.works-title-wrap
+  flex: 1
+  height: 100vh
+  position: sticky
+  top: 0
 
 .works-title
   position: absolute
-  left: 10%
-  top: 15rem
+  top: 50%
+  left: 50%
+  transform: translate(-50%,-50%)
   font-size: 5rem
   width: 30rem
   line-height: 30rem
 
-.work
-  position: absolute
-  width: 25rem
-  height: 25rem
+.work-wrap
+  height: 200vh
+  flex: 1
+
+.work1-wrap
+  height: 100vh
 
 .work1
-  top: 30rem
-  right: 10%
+  margin:
+    top: 30rem
+    right: 10%
 
 .work2
-  top: 60rem
-  left: 40%
+  margin:
+    top: 60rem
+    left: 40%
 </style>
